3289db21d26af1c9417327df1c5e7b0a267057d7
louisaldorio <louisaldorio@gmail.com>
2021-08-29 15:03:17 +0700

    Initial Commit

---
+++ service-one/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+)
+
+func homePage(w http.ResponseWriter, r *http.Request){
+    fmt.Fprintf(w, "Welcome to the Service 1!")
+}
+
+func handleRequests() {
+    http.HandleFunc("/", homePage)
+    log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func main() {
+    handleRequests()
+}
